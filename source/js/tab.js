672d047cd73f4ee5b7bdb139ea8e3884280577a6
nikas <nikasampetova@gmail.com>
2021-01-28 19:10:14 +0300

    add vendor file

--- source/js/tab.js
+++ source/js/tab.js
@@ -10,18 +10,28 @@
   var tabs = document.querySelectorAll('.tab');
   if (mainInfo && tabs) {
     mainInfo.classList.remove(NO_JS_TAG);
+    var closeAllTabs = function () {
+      [].slice.call(tabs).forEach(function (tab) {
+        tab.classList.remove(TAB_OPEN_TAG);
+        tab.classList.add(TAB_CLOSE_TAG);
+      })
+    }
     var onClickTabButton = function (button) {
+
       var closestButton = button.closest('.' + TAB_TAG);
       if (closestButton.classList.contains(TAB_CLOSE_TAG)) {
+        closeAllTabs();
         closestButton.classList.remove(TAB_CLOSE_TAG);
         closestButton.classList.add(TAB_OPEN_TAG);
       } else {
+
         closestButton.classList.remove(TAB_OPEN_TAG);
         closestButton.classList.add(TAB_CLOSE_TAG);
       }
     };
+
+    closeAllTabs();
     [].slice.call(tabs).forEach(function (tab) {
-      tab.classList.add(TAB_CLOSE_TAG);
       tab.addEventListener('click', function (evt) {
         if (evt.target.classList.contains(BUTTON_TAG)) {
           onClickTabButton(evt.target);
